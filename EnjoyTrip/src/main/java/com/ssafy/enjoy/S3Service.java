2a61dd22bfe35e9809f1fa087210dce73464388a
박근수 <baggeunsu@Moonjar.310>
2023-11-17 13:37:58 +0900

    s3 upload

--- EnjoyTrip/src/main/java/com/ssafy/enjoy/S3Service.java
+++ EnjoyTrip/src/main/java/com/ssafy/enjoy/S3Service.java
@@ -1,58 +1,59 @@
 package com.ssafy.enjoy;
 
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.ObjectMetadata;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Random;
+
 
 @RestController
 @RequestMapping("/s3")
 @RequiredArgsConstructor
 public class S3Service {
 
-    private final AmazonS3Client amazonS3Client;
+    private final S3Client s3Client;
 
     @Value("${aws.s3.bucket}")
     private String bucket;
 
     @GetMapping("test")
-    public void test(){
+    public void test() {
         System.out.println("s3 receive");
     }
 
     @PostMapping("upload")
-    public String saveFile(@RequestParam("file") MultipartFile file) throws IOException {
-        String originalFilename = file.getOriginalFilename();
+    public String S3FileUpload(@RequestParam("file") MultipartFile file) {
+        try {
+            String fileName = file.getOriginalFilename();
+            String contentType = file.getContentType();
+            String path = "test";
 
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentLength(file.getSize());
-        metadata.setContentType(file.getContentType());
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .contentType(contentType)
+                    .key(path + fileName)
+                    .build();
 
-        amazonS3Client.putObject(bucket, originalFilename, file.getInputStream(), metadata);
-        String url = amazonS3Client.getUrl(bucket, originalFilename).toString();
-        System.out.println(url);
-        return url;
+//            System.out.println(file);
+            PutObjectResponse response = s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
+            System.out.println(response.sdkHttpResponse().statusText());
+            System.out.println(response.sdkHttpResponse().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
     }
-//    public String S3FileUpload(@RequestParam("file") MultipartFile file){
-//        System.out.println("upload");
-//        try{
-//            String fileName = file.getOriginalFilename();
-//            String fileUrl = "https://" + bucket + "/img" +  fileName;
-//            ObjectMetadata metaData = new ObjectMetadata();
-//            metaData.setContentType(file.getContentType());
-//            metaData.setContentLength(file.getSize());
-//            System.out.println("여긴?");
-//            amazonS3Client.putObject(bucket,fileName,file.getInputStream(),metaData);
-//            return fileUrl;
-//        } catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        System.out.println("error");
-//        return "error";
-//    }
+
+
 }
---
+++ EnjoyTrip/src/main/java/com/ssafy/enjoy/config/S3ClientConfig.java
@@ -0,0 +1,37 @@
+package com.ssafy.enjoy.config;
+
+
+import ch.qos.logback.core.spi.PropertyContainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Configuration
+public class S3ClientConfig {
+
+    @Autowired
+    Environment environment;
+    @Value("${aws.s3.access-key}")
+    private String accessKey;
+    @Value("${aws.s3.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public S3Client s3Client(){
+
+        AwsCredentials credentials = AwsBasicCredentials.create(accessKey,secretKey);
+        return S3Client.builder()
+                .region(Region.AP_NORTHEAST_2)
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
+
+}
