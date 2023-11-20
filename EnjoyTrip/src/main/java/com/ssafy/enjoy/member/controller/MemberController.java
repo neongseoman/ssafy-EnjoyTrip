02516960c5b7dc6890572ac0d830d2a2f6dd34a0
KimSiEun <sieun043@gmail.com>
2023-11-20 10:28:41 +0900

    ...

--- EnjoyTrip/src/main/java/com/ssafy/enjoy/member/controller/MemberController.java
+++ EnjoyTrip/src/main/java/com/ssafy/enjoy/member/controller/MemberController.java
@@ -7,7 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.enjoy.member.model.Member;
 import com.ssafy.enjoy.member.model.ModifyMember;
