00d38d2ddd1f363730aeb7b44467e0b16792b125
박근수 <baggeunsu@Moonjar.310>
2023-11-21 19:31:55 +0900

    interceptor request valid check

--- EnjoyTrip/src/main/java/com/ssafy/enjoy/interceptor/UserInterceptor.java
+++ EnjoyTrip/src/main/java/com/ssafy/enjoy/interceptor/UserInterceptor.java
@@ -18,25 +18,31 @@ public class UserInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String reqBody = (String) request.getAttribute("requestBody");
         if ("POST".equals(request.getMethod())) {
-            if (reqBody != null) {
+            if (request.getAttribute("requestBody") == null) // if post method,req must have body.
+                return false;
+            String reqBody = (String) request.getAttribute("requestBody");
+            if (!("".equals(reqBody)) ) { // if reqBody is not empty
                 System.out.println("interceptor reqBody : " + reqBody);
                 JsonParser jsonParser = new JsonParser();
                 Object obj = jsonParser.parse(reqBody);
                 JsonObject jsonObj = (JsonObject) obj;
 
-                String jsonObjSessionId = String.valueOf(jsonObj.get("sessionId"));
-                String userSessionId = jsonObjSessionId.substring(1, jsonObjSessionId.length() - 1);
+                if (jsonObj.get("sessionId") != null) {
+                    String jsonObjSessionId = String.valueOf(jsonObj.get("sessionId"));
+                    String userSessionId = jsonObjSessionId.substring(1, jsonObjSessionId.length() - 1);
 
-                if (sessionService.isSessionValid(userSessionId)) {
+                    if (sessionService.isSessionValid(userSessionId)) {
 //                    Enumeration<String> e = request.getHeaderNames();
 //                    while (e.hasMoreElements()) {
 //                        String n = e.nextElement();
 //                        System.out.println(n + " : " + request.getHeader(n));
-                    System.out.println("interceptor pass!");
-                    return true;
-                } else { // if session is Valid return true, else return false
+                        System.out.println("interceptor pass!");
+                        return true;
+                    } else { // if session is Valid return true, else return false
+                        return false;
+                    }
+                } else{
                     return false;
                 }
             } else { // if requestBody is null return false. if send post, must bring body.
